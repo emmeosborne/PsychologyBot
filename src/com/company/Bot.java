658e55dc175c7f000b446a142fd08238d5721724
osborne.emme <emmeosborne12@gmail.com>
2017-12-15 08:47:59 -0800

    Latest version

--- src/com/company/Bot.java
+++ src/com/company/Bot.java
@@ -1,17 +1,52 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+//class ScannerTest{
+//    public static void main(String args[]){
+//        Scanner sc=new Scanner(System.in);
+//
+//        System.out.println("Enter your rollno");
+//        int rollno=sc.nextInt();
+//        System.out.println("Enter your name");
+//        String name=sc.next();
+//        System.out.println("Enter your fee");
+//        double fee=sc.nextDouble();
+//        System.out.println("Rollno:"+rollno+" name:"+name+" fee:"+fee);
+//        sc.close();
+//    }
+//}
+//
 public class Bot {
     private String name;
+    private ArrayList<String> greetings;
 
     public Bot() {
-        name = "default bot";
+        this("default Bot");
+
     }
 
     public Bot(String name) {
+        greetings = new ArrayList<String>();
+        greetings.add("Feeling down?");
+        greetings.add("Hows it going?");
+        greetings.add("ASUH");
         this.name = name;
     }
 
     public String getName() {
         return "Ms" + name;
+    }
+
+    public String greet() {
+        return "Hello my name is " + name;
+    }
+
+    public String randomGreeting() {
+        Random rand = new Random();
+        int which = rand.nextInt(greetings.size());
+        return greetings.get(which);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         Bot firstBot = new Bot("Sydney");
+
+
         System.out.println(firstBot.getName());
+        System.out.println(firstBot.greet());
+        System.out.println(firstBot.randomGreeting());
     }
+
 }
+
+
